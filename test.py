1c2b08467e0d8de2bd6862ea43b6f1242407881b
IvanovVlad123 <90443187+IvanovVlad123@users.noreply.github.com>
2021-12-17 12:11:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -11,15 +11,19 @@ class Test(unittest.TestCase):
     def test_2(self):
         p1=player(1280-5,800//2)
         p2=player(5,800//2)
-        bal = ball(Vector2(1279,100))
+        bal = ball(Vector2(10,799))
+        bal.s=Vector2(0,1)
         bal.update(1,p1,p2,1280,800)
-        self.assertEqual(bal.b, Vector2(1279.3, 100.3))
+        bal.update(7,p1,p2,1280,800)
+        self.assertEqual(bal.b, Vector2(10, 793))
     def test_3(self):
         p1=player(1280-5,800//2)
         p2=player(5,800//2)
-        bal = ball(Vector2(5,800//2))
+        bal = ball(Vector2(6,400))
+        bal.s=Vector2(-1,0)
         bal.update(1,p1,p2,1280,800)
-        self.assertEqual(bal.b, Vector2(5.3, 400.3))
+        bal.update(2,p1,p2,1280,800)
+        self.assertEqual(bal.b, Vector2(7, 400))
     def test_4(self):
         p1=player(5,400)
         p1.s = -1
